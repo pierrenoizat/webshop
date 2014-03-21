5196ac760f82acf38c3ad6c9aa7b0691d86184aa
Pierre Noizat <noizat@hotmail.com>
2014-03-21 16:42:21 +0100

    production env change

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -31,8 +31,8 @@ Depot::Application.configure do
   # In production, Apache or nginx will already do this
   # config.serve_static_assets = false
   config.serve_static_assets = true # required to serve the pdf invoice file
-  config.action_controller.perform_caching = true
-  config.assets.digest = true
+  # config.action_controller.perform_caching = true
+  # config.assets.digest = true
   # Enable serving of images, stylesheets, and javascripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
 
